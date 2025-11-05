f6b734d6613dc40dcce6dd433644c7751feace54
Aliaksandr Tsviatkou <aliaksandr.tsviatkou@sap.com>
2025-11-05 19:23:25 +0100

    Add validation to dtos

--- src/main/java/com/lms/mentoring/course/dto/CourseDto.java
+++ src/main/java/com/lms/mentoring/course/dto/CourseDto.java
@@ -1,5 +1,7 @@
 package com.lms.mentoring.course.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,12 +16,24 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class CourseDto {
+
     private UUID id;
+
+    @NotBlank(message = "Course title is required")
+    @Size(max = 255, message = "Title cannot exceed 255 characters")
     private String title;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
     private BigDecimal price;
+
+    @DecimalMin(value = "0.0", inclusive = true, message = "Coins paid cannot be negative")
     private BigDecimal coinsPaid;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+
+    @Valid
     private CourseSettingsDto settings;
 }
--- src/main/java/com/lms/mentoring/course/dto/CourseSettingsDto.java
+++ src/main/java/com/lms/mentoring/course/dto/CourseSettingsDto.java
@@ -1,5 +1,6 @@
 package com.lms.mentoring.course.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,15 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class CourseSettingsDto {
+
     private UUID id;
+
+    @FutureOrPresent(message = "Start date cannot be in the past")
     private LocalDateTime startDate;
+
+    @Future(message = "End date must be in the future")
     private LocalDateTime endDate;
+
+    @NotNull(message = "isPublic flag must be specified")
     private Boolean isPublic;
 }
